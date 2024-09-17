7537a024bf704d758e226a4aecec43906936aacb
nikola-al9 <nikola.ni@outlook.com>
2024-09-18 00:20:32 +0200

    connects players to same rooms

--- app/game/consumers.py
+++ app/game/consumers.py
@@ -2,9 +2,19 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class GameConsumer(AsyncWebsocketConsumer):
+    # Class-level dictionary to keep track of the number of players in each room
+    rooms = {}
+
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'game_{self.room_name}'
+
+        # Initialize room in the dictionary if it doesn't exist
+        if self.room_group_name not in GameConsumer.rooms:
+            GameConsumer.rooms[self.room_group_name] = 0
+
+        # Increment the player count
+        GameConsumer.rooms[self.room_group_name] += 1
 
         # Join room group
         await self.channel_layer.group_add(
@@ -13,12 +23,27 @@ class GameConsumer(AsyncWebsocketConsumer):
         )
         await self.accept()
 
-        # If there is only one player, send "Waiting for opponent"
-        await self.send(text_data=json.dumps({
-            'message': 'Waiting for opponent...'
-        }))
+        # Check the number of players in the room
+        if GameConsumer.rooms[self.room_group_name] == 1:
+            await self.send(text_data=json.dumps({
+                'message': 'Waiting for opponent...'
+            }))
+        elif GameConsumer.rooms[self.room_group_name] == 2:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'game_message',
+                    'message': 'Opponent found!'
+                }
+            )
 
     async def disconnect(self, close_code):
+        # Decrement the player count
+        if self.room_group_name in GameConsumer.rooms:
+            GameConsumer.rooms[self.room_group_name] -= 1
+            if GameConsumer.rooms[self.room_group_name] == 0:
+                del GameConsumer.rooms[self.room_group_name]
+
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
